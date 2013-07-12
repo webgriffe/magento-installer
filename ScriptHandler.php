94b1f255065a4f7adda8491c85dfea467cd14ea1
Manuele Menozzi <manuele.menozzi@gmail.com>
2013-07-12 08:54:23 +0200

    Initial commit with drafted ScriptHandler

---
+++ ScriptHandler.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Manuele Menozzi <mmenozzi@webgriffe.com>
+ */
+
+namespace Webgriffe\MagentoInstaller;
+
+
+use Composer\Script\Event;
+
+class ScriptHandler
+{
+    public static function installMagento(Event $event)
+    {
+
+    }
+}
